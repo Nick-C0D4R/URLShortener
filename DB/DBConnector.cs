a97d926d0a6ba9f677fca17612cf0a0a00eeddef
Mykola Kovshar <knikolay20031230@gmail.com>
2024-12-21 18:45:20 +0200

    Add static DBConnector

---
+++ DB/DBConnector.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SQLite;
+
+namespace DB
+{
+    public static class DBConnector
+    {
+        private static string _databaseName = "URLInfos.db";
+        private static string _path;
+        private static string _connectionString;
+        private static SQLiteConnection _connection;
+
+        public static void CreateDB(string path)
+        {
+            _path = path + "\\" + _databaseName;
+            SQLiteConnection.CreateFile(path);
+            _connectionString = $"Data Source={_path};Version=3;";
+        }
+
+        public static SQLiteConnection CreateConnection()
+        {
+            _connection = new SQLiteConnection(_connectionString);
+            _connection.Open();
+            return _connection;
+        }
+    }
+}
