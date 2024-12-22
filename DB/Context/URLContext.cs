24185810daeaacb961e4a7adf61093c72afa7607
Mykola Kovshar <knikolay20031230@gmail.com>
2024-12-22 12:52:43 +0200

    Add DBContext

---
+++ DB/Context/URLContext.cs
@@ -0,0 +1,28 @@
+﻿using DB.Connector;
+using DB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Context
+{
+    public class URLContext : DbContext
+    {
+        public DbSet<URLInfo> URLs { get; set; }
+        public DbSet<UserInfo> Users { get; set; }
+
+        public URLContext() : base()
+        {
+            DBConnector.CreateDB("C:\\Users\\Mykola\\source\\repos\\URLShortener\\DB");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
