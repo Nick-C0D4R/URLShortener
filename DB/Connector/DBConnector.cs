13c1d3240c8bd1d46787a76234927266b64aab4e
Mykola Kovshar <knikolay20031230@gmail.com>
2024-12-22 12:47:56 +0200

    Move DBConnector to Connector folder

---
+++ DB/Connector/DBConnector.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.SQLite;
+
+namespace DB.Connector
+{
+    public static class DBConnector
+    {
+        private static string _databaseName = "URLInfos.sqlite";
+        private static string _path;
+        private static string _connectionString;
+        private static SQLiteConnection _connection;
+
+        public static void CreateDB(string path)
+        {
+            _path = path + "\\" + _databaseName;
+            SQLiteConnection.CreateFile(_path);
+            _connectionString = $"Data Source={_path};";
+
+            using(_connection = new SQLiteConnection(_connectionString))
+            {
+                _connection.Open();
+                string commandText = "CREATE TABLE UserInfo (" +
+                    "UserId INT PRIMARY KEY," +
+                    "UserName VARCHAR(100) NOT NULL UNIQUE," +
+                    "Email TEXT NOT NULL UNIQUE," +
+                    "Password TEXT NOT NULL" +
+                    ");" +
+                    "" +
+                    "CREATE TABLE URLInfo (" +
+                    "UrlId INT PRIMARY KEY," +
+                    "UrlHash TEXT NOT NULL UNIQUE," +
+                    "ORIGINUrl TEXT NOT NULL UNIQUE," +
+                    "Created DATE NOT NULL," +
+                    "CreatedBy INT NOT NULL, " +
+                    "FOREIGN KEY(CreatedBy) REFERENCES UserInfo(UserId)" +
+                    ")";
+
+                using(var command = _connection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    command.ExecuteNonQuery();
+                }
+
+                _connection.Close();
+            }
+        }
+
+        public static SQLiteConnection CreateConnection()
+        {
+            _connection = new SQLiteConnection(_connectionString);
+            _connection.Open();
+            return _connection;
+        }
+    }
+}
