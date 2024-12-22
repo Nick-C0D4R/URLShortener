8f77f8d88e015e0f5a3fd86c2eea6ca71dc31cc5
Mykola Kovshar <knikolay20031230@gmail.com>
2024-12-22 12:49:39 +0200

    Add model for storing URL

---
+++ DB/Models/URLInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DB.Models
+{
+    [Table("URLInfo")]
+    public class URLInfo : BaseModel
+    {
+        [Required]
+        public string URL { get; set; }
+        [Required]
+        public string OriginalUrl { get; set; }
+        [Required]
+        public DateOnly Created { get; set; }
+
+        
+        public int CreatedId { get; set; }
+        public UserInfo Creator { get; set; }
+    }
+}
