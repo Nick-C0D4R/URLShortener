e7cb48f4e11410107d86f71938c9e5fff9bae22b
Mykola Kovshar <knikolay20031230@gmail.com>
2024-12-22 12:50:15 +0200

    Add model for storing users

---
+++ DB/Models/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DB.Models
+{
+    [Table("UserInfo")]
+    public class UserInfo : BaseModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public virtual ICollection<URLInfo> URLs { get; set; }
+    }
+}
